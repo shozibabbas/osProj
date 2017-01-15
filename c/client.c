57be548b1247439daee582666a897da5ae81dcfc
shozibabbas <14bscssabbas@seecs.edu.pk>
2017-01-15 13:40:45 +0500

    Add files via upload

---
+++ c/client.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<string.h>
+
+int main(int argc, char *argv[]) {
+	if (argc != 3)
+	{
+		printf("not enough arguments");
+		return 0;
+	}
+
+	char* fileName = argv[1];
+	int portNumber = atoi((char *)argv[2]);
+
+	char buff[2000];
+	char file_buffer[2000];
+	int sd, connfd, len;
+
+	struct sockaddr_in servaddr, cliaddr;
+
+	sd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	if (sd == -1)
+	{
+		printf(" socket not created in server\n");
+		exit(0);
+	}
+
+	bzero(&servaddr, sizeof(servaddr));
+
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(portNumber);
+
+	if (bind(sd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0)
+		printf("Not binded\n");
+
+	len = sizeof(cliaddr);
+
+	//recvfrom(sd, buff, 1024, 0,
+	//	(struct sockaddr *)&cliaddr, &len);
+
+	//printf("%s\n", buff);
+
+	FILE *fp;
+	fp = fopen("receivedFile.txt", "ab");
+	if (NULL == fp)
+	{
+		printf("\n Error opening file \n");
+	}
+
+	printf("waiting for server");
+	recvfrom(sd, buff, 500, 0, (struct sockaddr *) &cliaddr, &len);
+
+	printf("%s\n", buff);
+
+	return 1;
+}
---
+++ s/server.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include<netinet/in.h>
+#include<sys/types.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	if (argc != 3)
+	{
+		printf("not enough arguments");
+		return 0;
+	}
+
+	char* fileName = argv[1];
+	int portNumber = atoi((char *)argv[2]);
+
+	char buff[2000];
+	int sockfd, connfd, len;
+
+	struct sockaddr_in servaddr, cliaddr;
+	// create socket in client side
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	if (sockfd == -1)
+	{
+		printf(" socket not created in client\n");
+		exit(0);
+	}
+
+	bzero(&servaddr, sizeof(servaddr));
+
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = INADDR_ANY; // ANY address or use specific address
+	servaddr.sin_port = htons(portNumber);  // Port address
+
+	//printf("Type ur  UDP client message\n");
+	//scanf("%s", buff);
+
+	//// send  msg to server
+
+	//sendto(sockfd, buff, strlen(buff), 0,
+	//	(struct sockaddr *)&servaddr, sizeof(struct sockaddr));
+
+	/* Open the file that we wish to transfer */
+	FILE *fp = fopen("sample.txt", "rb");
+	if (fp == NULL)
+	{
+		printf("File open error");
+		return 1;
+	}
+
+	while (1) {
+		unsigned char buff[500] = { 0 };
+		int read_bytes = fread(buff, 1, 500, fp);
+
+		sendto(sockfd, buff, strlen(buff), 0, (struct sockaddr *) &servaddr, sizeof(struct sockaddr));
+
+		if (read_bytes < 500)
+		{
+			if (feof(fp))
+			{
+				printf("File has been sent...\n");
+				break;
+			}
+		}
+	}
+
+	return 1;
+}
